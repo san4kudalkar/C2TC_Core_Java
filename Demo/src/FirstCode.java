30f3b94d65adea7521ef38fa159388ce0de9f351
Sanik Kudalkar <Sanik Kudalkar@Sanik>
2022-01-28 11:09:23 +0530

    First Commit

---
+++ Demo/src/FirstCode.java
@@ -0,0 +1,12 @@
+
+public class FirstCode 
+{
+    public static void main(String[] args) 
+    {
+    	int[][][] x= {{{10,20,30},{40,50,60}},{{70,80},{90,100,110}}};
+    	String a= new String("AVANTIKA ");
+        int b=10, c=20, d=30; 
+    	System.out.println(b+a+c+d);
+    }
+}
+
